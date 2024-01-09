e6f9bdfc4935eaf6af79b58a0fec38f371170d2e
Yudah Mulia Nanang Sukabhakti <yudahmns@gmail.com>
2024-01-09 17:38:32 +0700

    - patch only update for not null - 400 for request validation - 404 for not found

--- Program.cs
+++ Program.cs
@@ -38,11 +38,18 @@ app.MapGet("/Movies", (MovieContext movies) => {
 app.MapGet("/Movies/{id}", (MovieContext movies, int id) => {
     var found = movies.movie.Find(id);
     if(found is null)
-        return Results.NotFound();
+        return Results.NotFound($"{id} not found");
     return Results.Ok(found);
 });
 
 app.MapPost("/Movies", (MovieContext movies, movie movie) => {
+    if (string.IsNullOrEmpty(movie.title))
+        return Results.BadRequest("title cannot be null or empty");
+    if (movie.rating is not null & 10 < movie.rating )
+        return Results.BadRequest("rating cannot be greater than 10");
+    if (movie.rating is not null & movie.rating < 0)
+        return Results.BadRequest("rating cannot be smaller than 0");
+
     var found = movies.movie.Find(movie.id);
     if (found is not null)
         return Results.Conflict(movie);
@@ -53,12 +60,22 @@ app.MapPost("/Movies", (MovieContext movies, movie movie) => {
 });
 
 app.MapMethods("/Movies/{id}", new string[] { "PATCH" }, (MovieContext movies, int id, movie movie) => {
+    if (movie.title is not null && movie.title == string.Empty)
+        return Results.BadRequest("title cannot be empty");
+    if (movie.rating is not null & 10 < movie.rating )
+        return Results.BadRequest("rating cannot be greater than 10");
+    if (movie.rating is not null & movie.rating < 0)
+        return Results.BadRequest("rating cannot be smaller than 0");
+
     var found = movies.movie.Find(id);
     if (found is null)
-        return Results.NotFound();
-    found.title = movie.title;
-    found.image = movie.image;
-    found.rating = movie.rating;
+        return Results.NotFound($"{id} not found");
+    if (!string.IsNullOrEmpty(movie.title))
+        found.title = movie.title;
+    if (!string.IsNullOrEmpty(movie.image))
+        found.image = movie.image;
+    if (movie.rating is not null)
+        found.rating = movie.rating;
     found.updated_at = DateTime.Now;
     movies.SaveChanges();
     return Results.NoContent();
@@ -67,7 +84,7 @@ app.MapMethods("/Movies/{id}", new string[] { "PATCH" }, (MovieContext movies, i
 app.MapDelete("/Movies/{id}", (MovieContext movies, int id) => {
     var found = movies.movie.Find(id);
     if (found is null)
-        return Results.NotFound();
+        return Results.NotFound($"{id} not found");
     movies.Remove(found);
     movies.SaveChanges();
     return Results.NoContent();
@@ -80,9 +97,11 @@ public class movie
     [SwaggerSchema(ReadOnly = true)]
     public int id { get; set; }
 
+    [Required]
     public string? title { get; set; }
 
-    public decimal rating { get; set; }
+    [Range(0, 10)]
+    public decimal? rating { get; set; }
 
     public string? image { get; set; }
 
